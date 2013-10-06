aba1809686a1b18cf697870a076385a3ea8fd7f1
Paco Ojka <patricksojka@gmx.net>
2013-10-06 21:06:12 +0200

    SpriteCollisionMapContainer.java Fertig muss nur nochmal getestet werden

--- Pixokalypse-Prototypes/src/com/we/PixokalypsePrototypes/test/SpriteCollisionMapContainer.java
+++ Pixokalypse-Prototypes/src/com/we/PixokalypsePrototypes/test/SpriteCollisionMapContainer.java
@@ -1,37 +1,50 @@
 package com.we.PixokalypsePrototypes.test;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-import com.badlogic.gdx.utils.Array;
 
 
 public class SpriteCollisionMapContainer {
 		TextureAtlas spriteSheet;
-		HashMap<String,Array<Integer>[][]> spriteCollisionmapHashmap;
+		HashMap<String,byte[][]> spriteCollisionmapHashmap;
 	public SpriteCollisionMapContainer(){
 		
 		spriteSheet = new TextureAtlas(Gdx.files.internal("data/heightmap/collisionpack.atlas"));
-		spriteCollisionmapHashmap = new HashMap<String, Array<Integer>[][]>();
+		spriteCollisionmapHashmap = new HashMap<String, byte[][]>();
 				
 		//Für alle definierten Spriteregions einen neuen Eintrag in der Hashmap anlegen
 		for(AtlasRegion item:spriteSheet.getRegions()){
 			String name = item.name;
-			//spriteCollisionmapHashmap.put(name, spriteToCollisionmapArray(spriteSheet.createSprite(name)));
-			//spriteHashmap.get(name).setSize(width, height);
+			System.out.println(item.name);
+			spriteCollisionmapHashmap.put(name, spriteToCollisionmapArray(spriteSheet.createSprite(name)));
 		}
-		System.out.println("anzahl Collisionmaps: " + spriteCollisionmapHashmap.size());
 	}
 	
-	private Array<Integer>[][] spriteToCollisionmapArray(Sprite createSprite) {
-		//magic
-		return null;
+	private byte[][] spriteToCollisionmapArray(Sprite createSprite) {
+		byte[][] collisionMap = new byte[createSprite.getRegionWidth()][createSprite.getRegionHeight()];
+		Pixmap pm = new Pixmap(createSprite.getRegionWidth(), createSprite.getRegionHeight(), createSprite.getTexture().getTextureData().getFormat());
+		TextureData td = createSprite.getTexture().getTextureData();
+		td.prepare();
+		pm = td.consumePixmap();
+		for(int x = 0; x < createSprite.getRegionWidth();x++){
+			for(int y = 0; y < createSprite.getRegionHeight();y++){
+				Color color = new Color(pm.getPixel(x, y));
+				if(color.r == 1 && color.b==1 && color.g == 1)collisionMap[x][y] = 0;
+				if(color.r == 0 && color.b==0 && color.g == 0)collisionMap[x][y] = 1;
+			}		
+		}
+		return collisionMap;
 	}
 
-	public Array<Integer>[][] getSpriteCollisionmap(String spriteName){
+	public byte[][] getSpriteCollisionmap(String spriteName){
 		return spriteCollisionmapHashmap.get(spriteName);
 	}
 }
